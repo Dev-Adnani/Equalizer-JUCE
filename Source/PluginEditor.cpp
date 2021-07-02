64f1d6df930ee24e078e007f653442ad9ecd6d4b
DevAdnani <Dev.adnani26@gmail.com>
2021-07-02 20:35:44 +0530

    Tweaked button text when enabled or disabled

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -53,10 +53,10 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g,
 
         r.setCentre(bounds.getCentre());
 
-        g.setColour(Colours::black);
+        g.setColour(enabled ? Colours::black : Colours::darkgrey);
         g.fillRect(r);
 
-        g.setColour(Colours::white);
+        g.setColour(enabled ? Colours::white : Colours::lightgrey);       
         g.drawFittedText(text, r.toNearestInt(), juce::Justification::centred, 1);
 
     }
