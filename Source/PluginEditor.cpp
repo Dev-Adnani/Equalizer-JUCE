8d1e47e33755d41e71b6133312363162f7108353
DevAdnani <Dev.adnani26@gmail.com>
2021-07-02 19:08:58 +0530

    Added Graphics For  Toggle button

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -59,8 +59,45 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g,
         g.drawFittedText(text, r.toNearestInt(), juce::Justification::centred, 1);
 
     }
+}
 
+void LookAndFeel::drawToggleButton(juce::Graphics& g,
+    juce::ToggleButton& toggleButton,
+    bool shouldDrawButtonAsHighlighted,
+    bool shouldDrawButtonAsDown)
+{
+    using namespace juce;
 
+    Path powerButton;
+
+    auto bounds = toggleButton.getLocalBounds();
+
+    auto size = jmin(bounds.getWidth(), bounds.getHeight()) - 6;
+    auto r = bounds.withSizeKeepingCentre(size, size).toFloat();
+
+    float ang = 30.f;
+
+    size -= 6;
+
+    powerButton.addCentredArc(r.getCentreX(),
+        r.getCentreY(),
+        size * 0.5,
+        size * 0.5,
+        0.f,
+        degreesToRadians(ang),
+        degreesToRadians(360.f - ang),
+        true);
+
+    powerButton.startNewSubPath(r.getCentreX(), r.getY());
+    powerButton.lineTo(r.getCentre());
+
+    PathStrokeType pst(2.f, PathStrokeType::JointStyle::curved);
+
+    auto color = toggleButton.getToggleState() ? Colours::dimgrey : Colour(255u, 152u, 0u);
+
+    g.setColour(color);
+    g.strokePath(powerButton, pst);
+    g.drawEllipse(r, 2);
 }
 
 void RotarySliderWithLabels::paint(juce::Graphics& g)
@@ -534,14 +571,18 @@ EqualizerJUCEAudioProcessorEditor::EqualizerJUCEAudioProcessorEditor(EqualizerJU
         addAndMakeVisible(comp);
     }
 
-
+    peakBypassButton.setLookAndFeel(&lnf);
+    lowcutBypassButton.setLookAndFeel(&lnf);
+    highcutBypassButton.setLookAndFeel(&lnf);
 
     setSize(600, 400);
 }
 
 EqualizerJUCEAudioProcessorEditor::~EqualizerJUCEAudioProcessorEditor()
 {
-
+    peakBypassButton.setLookAndFeel(nullptr);
+    lowcutBypassButton.setLookAndFeel(nullptr);
+    highcutBypassButton.setLookAndFeel(nullptr);
 }
 
 //==============================================================================
